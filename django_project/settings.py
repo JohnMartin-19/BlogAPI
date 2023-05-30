c681379d535f79be20eeefb8945f8242342476bf
JohnMartin-19 <johnnkonge2020@gmail.com>
2023-05-30 09:57:21 +0300

    BLOG API Setup

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,11 +37,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd party
+    "rest_framework",
+    "corsheaders",
+    #Local
+    "accounts.apps.AccountsConfig",
+    "posts.apps.PostsConfig",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSIONS_CLASSES":[
+        "rest_framework.permissions.AllowAny",
+    ],
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -49,6 +63,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 ROOT_URLCONF = 'django_project.urls'
 
 TEMPLATES = [
@@ -121,3 +141,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "accounts.CustomUser"
